3bc5a5a03b5069fcc6085c69560d1ad6db336274
Brandon Brown <afrobot@gmail.com>
2025-07-08 19:50:12 -0700

    Adds a class method and tests it

--- src/snipster/models.py
+++ src/snipster/models.py
@@ -5,3 +5,11 @@ class Snippet(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str
     code: str
+
+    @classmethod
+    # "Snippet" is a forward reference that allows
+    # type hints to reference the class from inside itself
+    def from_dict(cls, data: dict[str, str]) -> "Snippet":
+        title = data["title"]
+        code = data["code"]
+        return cls(title=title, code=code)
--- tests/test_model.py
+++ tests/test_model.py
@@ -15,15 +15,23 @@ def setup_database():
 
 
 def test_create_snippet():
-    # do something
     snippet = Snippet(title="Test Snippet", code="print('foo')")
     with Session(engine) as session:
-        SQLModel.metadata.create_all(engine)
         session.add(snippet)
         session.commit()
         session.refresh(snippet)
-    # hardcode expected results
-    # check the result with assert
     assert snippet.id is not None
     assert snippet.title == "Test Snippet"
     assert snippet.code == "print('foo')"
+
+
+def test_create_snippet_with_from_dict_cls_method():
+    snippet_dict = {"title": "Amazing Snippet", "code": "print('Be amazed!')"}
+    snippet = Snippet.from_dict(snippet_dict)
+    with Session(engine) as session:
+        session.add(snippet)
+        session.commit()
+        session.refresh(snippet)
+        assert snippet.id is not None
+        assert snippet.title == "Amazing Snippet"
+        assert snippet.code == "print('Be amazed!')"

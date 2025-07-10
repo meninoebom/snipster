5538b1e2ecd540afa9c7f8279528e0e372251a0f
Brandon Brown <afrobot@gmail.com>
2025-07-09 19:12:36 -0700

    Implementes repository pattern with db backed and in mememory versions of repo (WIP)

--- src/snipster/models.py
+++ src/snipster/models.py
@@ -1,23 +1,43 @@
+from typing import Optional
+
 from sqlmodel import Field, SQLModel
 
 
-class Snippet(SQLModel, table=True):
+class SnippetORM(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str
     code: str
 
-    @classmethod
-    # "Snippet" is a forward reference that allows
-    # type hints to reference the class from inside itself
-    def from_dict(cls, **kwargs) -> "Snippet":
-        if "title" not in kwargs:
-            raise ValueError("Missing required argument: 'title'")
-        if "code" not in kwargs:
-            raise ValueError("Missing required argument: 'code'")
-        if not isinstance(kwargs["title"], str):
-            raise TypeError("'title' must be a string")
-        if not isinstance(kwargs["code"], str):
-            raise TypeError("'code' must be a string")
-        if len(kwargs["title"]) < 3:
-            raise ValueError("Title must be at least 3 chars long")
-        return cls(**kwargs)
+
+#     @classmethod
+#     # "Snippet" is a forward reference that allows
+#     # type hints to reference the class from inside itself
+#     def from_dict(cls, **kwargs) -> "Snippet":
+#    if "title" not in kwargs:
+#         raise ValueError("Missing required argument: 'title'")
+#     if "code" not in kwargs:
+#         raise ValueError("Missing required argument: 'code'")
+#     if not isinstance(kwargs["title"], str):
+#         raise TypeError("'title' must be a string")
+#     if not isinstance(kwargs["code"], str):
+#         raise TypeError("'code' must be a string")
+#     if len(kwargs["title"]) < 3:
+#         raise ValueError("Title must be at least 3 chars long")
+#     return cls(**kwargs)
+
+
+class Snippet:
+    def __init__(self, id: Optional[int], title: str, code: str):
+        if not isinstance(title, str):
+            raise TypeError("`title` must be a string")
+        if len(title) < 3:
+            raise ValueError("`title` must be at least 3 characters long")
+
+        if not isinstance(code, str):
+            raise TypeError("`code` must be a string")
+        if not code.strip():
+            raise ValueError("`code` cannot be empty")
+
+        self.id = id
+        self.title = title
+        self.code = code
---
+++ src/snipster/repo.py
@@ -0,0 +1,96 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from sqlmodel import create_engine, select
+
+from .db import get_session
+from .models import Snippet, SnippetORM
+
+engine = create_engine("sqlite:///:memory:", echo=True)
+
+
+class AbstractSnippetRepo(ABC):
+    @abstractmethod
+    def add(self, snippet: Snippet) -> None:
+        pass
+
+    @abstractmethod
+    def get(self, snippet_id) -> Snippet | None:
+        pass
+
+    @abstractmethod
+    def list(self) -> Sequence[Snippet]:
+        pass
+
+    @abstractmethod
+    def delete(self, snippet_id: int) -> None:
+        pass
+
+
+class DatabaseBackedSnippetRepo(AbstractSnippetRepo):
+    def add(self, snippet: Snippet):
+        snippet_orm = SnippetORM(id=snippet.id, title=snippet.title, code=snippet.code)
+        with get_session() as session:
+            session.add(snippet_orm)
+            session.commit()
+            session.refresh(snippet_orm)
+
+    def get(self, snippet_id) -> Snippet | None:
+        with get_session() as session:
+            snippet_orm = session.get(SnippetORM, snippet_id)
+        if snippet_orm:
+            return Snippet(
+                id=snippet_orm.id, title=snippet_orm.title, code=snippet_orm.code
+            )
+
+    def list(self) -> Sequence[Snippet]:
+        with get_session() as session:
+            snippet_orms = session.exec(select(SnippetORM)).all()
+            return [
+                Snippet(id=orm.id, title=orm.title, code=orm.code)
+                for orm in snippet_orms
+            ]
+
+    def delete(self, snippet_id: int):
+        with get_session() as session:
+            snippet = session.get(SnippetORM, snippet_id)
+            if snippet:
+                session.delete(snippet)
+                session.commit()
+
+
+class InMemorySnippetRepo(AbstractSnippetRepo):
+    def __init__(self):
+        self.snippets: dict[int, Snippet] = {}
+
+    def add(self, snippet: Snippet) -> None:
+        if snippet.id is None:
+            snippet.id = max(self.snippets.keys(), default=0) + 1
+        self.snippets[snippet.id] = snippet
+
+    def get(self, snippet_id) -> Snippet | None:
+        snippet = self.snippets[snippet_id]
+        if snippet:
+            return snippet
+        else:
+            return None
+
+    def list(self) -> Sequence[Snippet]:
+        return list(self.snippets.values())
+
+    def delete(self, snippet_id: int) -> None:
+        del self.snippets[snippet_id]
+
+
+class JsonBackedSnippetRepo(AbstractSnippetRepo):
+    def add(self, snippet: Snippet) -> None:
+        pass
+
+    def get(self, snippet_id) -> Snippet | None:
+        pass
+
+    def list(self) -> Sequence[Snippet]:
+        pass
+
+    def delete(self, snippet_id: int) -> None:
+        pass

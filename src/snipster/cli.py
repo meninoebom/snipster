d41276924b0b0ed97d72867f47df00e633dbff62
Brandon Brown <afrobot@gmail.com>
2025-07-27 14:05:03 -0700

    Sets up arguments and options for CLI commands

--- src/snipster/cli.py
+++ src/snipster/cli.py
@@ -1,33 +1,82 @@
-from typer import Typer
+from enum import Enum
 
-app = Typer()
-
-
-@app.command()
-def list():
-    print("list")
+import typer
+from typing_extensions import Annotated
 
 
-@app.command()
-# argumenst + title, code, description
-def add():
-    print("add")
+class Language(str, Enum):
+    javascript = "javascript"
+    python = "python"
+    rust = "rust"
 
 
-@app.command()
-def delete():
-    print("delete")
+app = typer.Typer()
 
 
 @app.command()
-def toggle_favorite():
-    print("favorite")
+def add(
+    title: Annotated[
+        str, typer.Option(..., "--title", "-t", help="Title of the snippet")
+    ],
+    code: Annotated[str, typer.Option(..., "--code", "-c", help="The snippet code")],
+    language: Annotated[
+        Language,
+        typer.Option(
+            ...,
+            "--language",
+            "--lang",
+            "-l",
+            help="Which language is your snippet written in? Javascript, Python, or Rust",
+            case_sensitive=False,
+            prompt="Choose a language",
+            show_choices=True,
+        ),
+    ],
+    description: Annotated[
+        str,
+        typer.Option(
+            ...,
+            "--description",
+            "--desc",
+            "-d",
+            help="The snippet code",
+            rich_help_panel="Optional",
+        ),
+    ] = "",
+):
+    """
+    Add a new code snippet to the repository.
+    """
+    print(f"title = {title}")
+    print(f"code = {code}")
+    print(f"description = {description}")
+    print(f"language = {language}")
 
+    @app.command()
+    def list():
+        """
+        List all snippets
+        """
+        print("list")
 
-@app.command()
-def search():
-    print("search")
+    @app.command()
+    def toggle_favorite():
+        print("favorite")
 
+    @app.command()
+    def search(
+        query: Annotated[
+            str,
+            typer.Argument(help="Search for snippets by title, description, or tags"),
+        ],
+    ):
+        print(f"searching for {query}")
 
-if __name__ == "__main__":
-    app()
+    @app.command()
+    def delete(
+        id: Annotated[int, typer.Argument(help="The ID of the snippet to delete")],
+    ):
+        print(f"deleting snipper with id{id}")
+
+    if __name__ == "__main__":
+        app()

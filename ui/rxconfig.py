f9c00d20f49b6c5315c8213c5f294d8bb1cce9d7
Brandon Brown <afrobot@gmail.com>
2025-08-15 07:59:42 -0700

    Remove hardcoded ports from rx.Config (experiment to make deploy work)

--- ui/rxconfig.py
+++ ui/rxconfig.py
@@ -1,7 +1,8 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="ui",  # Use a unique name that won't conflict
-    frontend_port=3000,
-    backend_port=8001,
+    app_name="ui",
+    # Remove these hardcoded ports:
+    # frontend_port=3000,
+    # backend_port=8001,
 )
